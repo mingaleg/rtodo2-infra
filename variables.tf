9f503f7f4c6569bb4429e38adbd90865cbc4e329
Oleg Mingalev <oleg@mingalev.net>
2023-04-11 20:35:34 +0100

    Configure AWS RDS

--- variables.tf
+++ variables.tf
@@ -17,3 +17,21 @@ variable "ecs_prod_backend_retention_days" {
   description = "Retention period for backend logs (days)"
   default     = 365
 }
+
+# RDS
+
+variable "prod_rds_db_name" {
+  description = "RDS database name"
+  default     = "rtodo2"
+}
+variable "prod_rds_username" {
+  description = "RDS database username"
+  default     = "rtodo2"
+}
+variable "prod_rds_password" {
+  description = "postgres password for production DB"
+}
+variable "prod_rds_instance_class" {
+  description = "RDS instance type"
+  default     = "db.t4g.micro"
+}
