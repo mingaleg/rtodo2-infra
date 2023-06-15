8b4f519cfc640cef9b97ad6cdc290968d02a6535
Oleg Mingalev <oleg@mingalev.net>
2023-06-15 02:50:25 +0100

    health check: /admin/login (302) -> /admin/login/ (200)

--- load_balancer.tf
+++ load_balancer.tf
@@ -16,7 +16,7 @@ resource "aws_lb_target_group" "prod_backend" {
   target_type = "ip"
 
   health_check {
-    path                = "/admin/"
+    path                = "/admin/login/"
     port                = "traffic-port"
     healthy_threshold   = 5
     unhealthy_threshold = 2
